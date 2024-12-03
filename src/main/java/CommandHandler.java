3b171ead20660770f1bbf8f7bfa098c32245b39a
dvir <dvirm.biton@gmail.com>
2024-12-03 21:47:46 +0200

    Added type builtin function

---
+++ src/main/java/CommandHandler.java
@@ -0,0 +1,15 @@
+public class CommandHandler {
+    public static Command parseCommand(String command) {
+        try {
+            return Command.valueOf(command.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    public enum Command {
+        ECHO,
+        TYPE,
+        EXIT
+    }
+}
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -4,9 +4,14 @@ public class Parser {
     public String parseInput(final String input) {
         final Scanner parserScanner = new Scanner(input);
 
-        final String command = parserScanner.next();
+        final String commandStr = parserScanner.next();
+        final CommandHandler.Command command = CommandHandler.parseCommand(commandStr);
+        if (command == null) {
+            return commandStr + ": command not found";
+        }
+
         return switch (command) {
-            case "echo" -> {
+            case CommandHandler.Command.ECHO -> {
                 if (!parserScanner.hasNext()) {
                     yield "";
                 }
@@ -14,8 +19,17 @@ public class Parser {
                 // to eliminate the space
                 yield parserScanner.nextLine().substring(1);
             }
-            case "exit" -> null;
-            default -> command + ": command not found";
+            case TYPE -> {
+                final String argument = parserScanner.next();
+                if (CommandHandler.parseCommand(argument) != null) {
+                    yield argument + " is a shell builtin";
+                } else {
+                    yield argument + ": not found";
+                }
+            }
+            case CommandHandler.Command.EXIT -> null;
         };
     }
+
+
 }
